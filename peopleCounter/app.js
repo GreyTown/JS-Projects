e93b5cbb15bb219f97be709fcd09baecac0c74c9
GreyTown <aggreykanyoro@gmail.com>
2023-06-20 23:15:42 +0300

    working on the waether app

--- peopleCounter/app.js
+++ peopleCounter/app.js
@@ -1,6 +1,17 @@
+const increment_btn = document.getElementById('increment-btn');
+const placeHolder = document.getElementById('hech1');
 
- let count = 0
+let count = 0;
 
- document.getElementById('hech1').innerText = count 
-
- 
+// 
+function addCount() {
+        count ++;
+     } 
+increment_btn.onclick = () => {
+     addCount()
+}    
+function printNumber(num) {
+     placeHolder.innerHTML = num
+}
+let result = addCount()
+printNumber(result)
