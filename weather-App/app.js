1760a9fcd9365999b2280ab5bba2d422c11916ba
GreyTown <aggreykanyoro@gmail.com>
2023-06-29 12:05:50 +0300

    linked the css file

---
+++ weather-App/app.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container');
+const search = document.getElementById('button');
+const weatherBox = document.getElementById('weather-box');
+const weatherDetails = document.getElementById('weather-details');
+const error404 = document.getElementById('not-found');
+
+search.addEventListener("click",() => {
+    console.log('hello world');
+})
+
